6d9c4bfdaad74a894028a11587e5f1b4d0a27242
Savannah Jones <savannah.n.jones7@gmail.com>
2025-10-16 00:20:51 -0500

    idle monster left/right direction

--- maze-game/Scripts/Seeker.gd
+++ maze-game/Scripts/Seeker.gd
@@ -1,25 +1,43 @@
 extends CharacterBody2D
 class_name Monster
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+
+const SPEED = 400.0
+#const SPRINT_SPEED = 600
+var last_direction := Vector2(1,0)
 
 
 func _physics_process(delta: float) -> void:
-	# Gravity? Don't think its needed.
-	#if not is_on_floor():
-		#velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = direction * SPEED
+	
 	move_and_slide()
+	#normalize the vector to account for diagonal movement later maybe?
+	if direction.length() > 0:
+		last_direction = direction
+		play_walk_animation(direction)
+		print(direction)
+	else:
+		play_idle_animation(last_direction)
+		print(direction)
+
+func play_walk_animation(direction):
+	if direction.x > 0:
+		$AnimatedSprite2D.play("walk_right")
+	elif direction.x > 0:
+		$AnimatedSprite2D.play("walk_left")
+	elif direction.y > 0:
+		$AnimatedSprite2D.play("walk_down")
+	elif direction.y < 0:
+		$AnimatedSprite2D.play("walk_up")
+
+func play_idle_animation(direction):
+	if direction.x > 0:
+		$AnimatedSprite2D.play("idle_right")
+	elif direction.x > 0:
+		$AnimatedSprite2D.play("idle_left")
+	elif direction.y > 0:
+		$AnimatedSprite2D.play("idle_down")
+	elif direction.y < 0:
+		$AnimatedSprite2D.play("idle_up")
