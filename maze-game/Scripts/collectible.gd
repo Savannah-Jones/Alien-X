06bd8b9ff1f23858067b62c4312b0eae072029d5
Savannah Jones <savannah.n.jones7@gmail.com>
2025-10-16 01:01:12 -0500

    collectible png added

--- maze-game/Scripts/collectible.gd
+++ maze-game/Scripts/collectible.gd
@@ -6,7 +6,7 @@ var itemsCollected = 0;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	anim.play("idle")
 
 
 func _on_body_entered(body: Node2D) -> void:
