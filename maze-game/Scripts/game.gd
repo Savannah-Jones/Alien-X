c56a23e50061271717000bff800a5c381c2e8534
Savannah Jones <savannah.n.jones7@gmail.com>
2025-11-03 12:33:41 -0600

    fixed viewport

--- maze-game/Scripts/game.gd
+++ maze-game/Scripts/game.gd
@@ -17,7 +17,24 @@ extends Node
 @onready var runner := $HBoxContainer/SubViewportContainer/SubViewport/Level/Runner
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	await get_tree().process_frame
+	
+	var main_view_size = get_viewport().size  # e.g. (1920,1080)
+	var half_width = int(main_view_size.x / 2)
+	
+	players["1"].viewport.size = Vector2(half_width, main_view_size.y)
+	players["2"].viewport.size = Vector2(half_width, main_view_size.y)
+	
 	players["2"].viewport.world_2d = players["1"].viewport.world_2d
+	
+	
+	for c in get_tree().get_nodes_in_group("Collectibles"):
+		print("Collectible:", c.name, "at", c.global_position)
+		var marker = ColorRect.new()
+		marker.color = Color(1, 0, 0, 0.5)
+		marker.size = Vector2(8, 8)
+		add_child(marker)
+		marker.global_position = c.global_position
 	
 	for node in players.values():
 		var remote_transform := RemoteTransform2D.new()
