1df714d70d0c409a37c5feb712cafbfbf05da21b
RMW150 <jdledb6@gmail.com>
2025-11-03 10:38:13 -0600

    Add files via upload

---
+++ maze-game/Scripts/main_menu.gd
@@ -0,0 +1,26 @@
+extends Control
+
+@onready var main_buttons: VBoxContainer = $Node/MainButtons
+@onready var options: Panel = $AudioStreamPlayer/Options
+
+
+func _ready():
+	main_buttons.visible = true
+	options.visible = false
+
+func _on_start_pressed() -> void:
+	#This is where you would put the main scene level for the game in the ("").
+	get_tree().change_scene_to_file("")
+
+
+func _on_settings_pressed() -> void:
+	main_buttons.visible = false
+	options.visible = true
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_back_options_pressed() -> void:
+	_ready()
